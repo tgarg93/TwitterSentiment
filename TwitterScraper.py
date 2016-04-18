c0707457da3ec3e4d10edf0af21373b7e4e1e2b8
tgarg93 <tushargarg93@gmail.com>
2016-04-18 12:11:55 -0400

    Tweepy Auth

--- TwitterScraper.py
+++ TwitterScraper.py
@@ -1,5 +1,20 @@
 __author__ = 'tushar'
 
 import tweepy
+import json
 
+credentials = json.loads(open("credentials.json").read())
 
+consumer_key = credentials["consumerKey"]
+consumer_secret = credentials["consumerSecret"]
+access_token = credentials["accessToken"]
+access_token_secret = credentials["accessSecret"]
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print tweet.text
