91b326899a078eb3130518b87016d0e49cf9779d
tgarg93 <tushargarg93@gmail.com>
2016-04-18 23:31:50 -0400

    Created mapping of politicians to screen names

--- TwitterScraper.py
+++ TwitterScraper.py
@@ -15,6 +15,30 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print tweet.text
+# Map of politician name to Twitter screen name
+politicianScreeNames = dict()
+
+
+def populatePresidentialCandidates():
+    # Republican candidates
+    politicianScreeNames["Donal Trump"] = "@realDonaldTrump"
+    politicianScreeNames["Ted Cruz"] = "@tedcruz"
+    politicianScreeNames["John Kasich"] = "@JohnKasich"
+    # Democratic candidates
+    politicianScreeNames["Bernie Sanders"] = "@BernieSanders"
+    politicianScreeNames["Hilary Clinton"] = "@HilaryClinton"
+    # Other important politicians
+    politicianScreeNames["Barack Obama"] = "@BarackObama"
+    politicianScreeNames["Paul Ryan"] = "@SpeakerRyan"
+    politicianScreeNames["Nancy Pelosi"] = "@NancyPelosi"
+    politicianScreeNames["Lindsey Graham"] = "@GrahamBlog"
+    politicianScreeNames["Harry Reid"] = "@SenatorReid"
+    politicianScreeNames["Elizabeth Warren"] = "@SenWarren"
+
+
+if __name__ == '__main__':
+    populatePresidentialCandidates()
+    for name, screen_name in politicianScreeNames.iteritems():
+        new_tweets = api.user_timeline(screen_name=screen_name, count=200)
+        print name, new_tweets[0].text.encode("utf-8")
+
